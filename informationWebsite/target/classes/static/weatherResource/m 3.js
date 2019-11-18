17c58030f0e2d83fd7561baf633876dbb3d40468
cheng tian <chengtian@chengdeMacBook-Air.local>
2019-11-18 12:17:16 -0500

    version 1.0

---
+++ informationWebsite/target/classes/static/weatherResource/m 3.js
@@ -0,0 +1,8 @@
+(function() {
+                            var s = "_" + Math.random().toString(36).slice(2);
+                            document.write('<div style="" id="' + s + '"></div>');
+                            (window.slotbydup = window.slotbydup || []).push({
+                                id: "u4178710",
+                                container:  s
+                            });
+                        })();
---
+++ informationWebsite/target/classes/static/weatherResource/q 2.js
@@ -0,0 +1,10 @@
+(function() {
+    var s = "_" + Math.random().toString(36).slice(2);
+    document.write('<div id="' + s + '"></div>');
+    (window.slotbydup=window.slotbydup || []).push({
+        id: '6344623',
+        container: s,
+        size: '300,250',
+        display: 'inlay-fix'
+    });
+})();
